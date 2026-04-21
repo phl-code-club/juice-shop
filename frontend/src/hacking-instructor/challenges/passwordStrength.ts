718eb23756a8a065a6979bdde3fb41bf7d5d6e9b
ajstensland <35818081+ajstensland@users.noreply.github.com>
2026-04-21 14:37:30 -0400

    Moved tutorial to dodge weird rendering issue for Password Strength challenge

--- frontend/src/hacking-instructor/challenges/passwordStrength.ts
+++ frontend/src/hacking-instructor/challenges/passwordStrength.ts
@@ -25,56 +25,56 @@ export const PasswordStrengthInstruction: ChallengeInstruction = {
     {
       text:
         "In this challenge we'll try to log into the administrator's user account using his original credentials.",
-      fixture: 'app-navbar',
+      fixture: '#navbarAccount',
       resolved: waitInMs(7000)
     },
     {
       text:
         "If you don't know it already, you must first find out the admin's email address. The user feedback and product reviews are good places to look into. When you have it, go to the _Login_ page.",
-      fixture: 'app-navbar',
+      fixture: '#navbarAccount',
       fixtureAfter: true,
       unskippable: true,
       resolved: waitForAngularRouteToBeVisited('login')
     },
     {
       text: "Enter the admin's email address into the **email field**.",
-      fixture: '#email',
+      fixture: '#navbarAccount',
       unskippable: true,
       resolved: waitForInputToHaveValue('#email', 'admin@juice-sh.op', { replacement: ['juice-sh.op', 'application.domain'] })
     },
     {
       text: 'Now for the password. Lucky for us, the admin chose a really, really, **really** stupid one. Just try any that comes to your mind!',
-      fixture: '#password',
+      fixture: '#navbarAccount',
       unskippable: true,
       resolved: waitForInputToNotBeEmpty('#password')
     },
     {
       text: "🤦‍♂️ Nah, that was wrong! Keep trying! I'll tell you when you're one the right track.",
-      fixture: '#password',
+      fixture: '#navbarAccount',
       unskippable: true,
       resolved: waitForInputToHaveValue('#password', 'admin')
     },
     {
       text: 'Okay, you are one the right track, but this would have been the worst password in the world for an admin. He spiced it up a little bit with some extra non-letter characters. Keep trying!',
-      fixture: '#password',
+      fixture: '#navbarAccount',
       unskippable: true,
       resolved: waitForInputToHaveValue('#password', 'admin1')
     },
     {
       text: "🔥 Yes, it's getting warmer! Try adding some more numbers maybe?",
-      fixture: '#password',
+      fixture: '#navbarAccount',
       unskippable: true,
       resolved: waitForInputToHaveValue('#password', 'admin12')
     },
     {
       text: "🧯 It's getting hot! Just one more digit...",
-      fixture: '#password',
+      fixture: '#navbarAccount',
       unskippable: true,
       resolved: waitForInputToHaveValue('#password', 'admin123')
     },
     {
       text: 'Okay, now press the _Log in_ button.',
-      fixture: '#rememberMe',
+      fixture: '#navbarAccount',
       unskippable: true,
       resolved: waitForElementToGetClicked('#loginButton')
     },
