c6a67e61aff77bdfa38814a7dbbfd88fd6922143
ajstensland <35818081+ajstensland@users.noreply.github.com>
2026-05-01 17:10:37 -0400

    Fixed SECURITY/security issue

--- routes/verify.ts
+++ routes/verify.ts
@@ -67,7 +67,7 @@ export const accessControlChallenges = () => ({ url }: Request, res: Response, n
   challengeUtils.solveIf(challenges.privacyPolicyChallenge, () => { return utils.endsWith(url, '/81px.png') })
   challengeUtils.solveIf(challenges.extraLanguageChallenge, () => { return utils.endsWith(url, '/tlh_AA.json') })
   challengeUtils.solveIf(challenges.retrieveBlueprintChallenge, () => { return utils.endsWith(url, retrieveBlueprintChallengeFile ?? undefined) })
-  challengeUtils.solveIf(challenges.securityPolicyChallenge, () => { return utils.endsWith(url, '/security.txt') })
+  challengeUtils.solveIf(challenges.securityPolicyChallenge, () => { return utils.endsWith(url.toLowerCase(), '/security.txt') })
   challengeUtils.solveIf(challenges.missingEncodingChallenge, () => { return utils.endsWith(url.toLowerCase(), '%e1%93%9a%e1%98%8f%e1%97%a2-%23zatschi-%23whoneedsfourlegs-1572600969477.jpg') })
   challengeUtils.solveIf(challenges.accessLogDisclosureChallenge, () => { return url.match(/access\.log(0-9-)*/) })
   next()
