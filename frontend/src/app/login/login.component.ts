ff0bfd97a497ea594b9bce3eac1b7b9009a1ebc0
ajstensland <35818081+ajstensland@users.noreply.github.com>
2026-05-02 10:56:32 -0400

    Disabled Google login

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -75,15 +75,7 @@ export class LoginComponent implements OnInit {
     this.configurationService.getApplicationConfiguration().subscribe({
       next: (config) => {
         if (config?.application?.googleOauth) {
-          this.clientId = config.application.googleOauth.clientId
-          const authorizedRedirect = config.application.googleOauth.authorizedRedirects.find(r => r.uri === this.redirectUri)
-          if (authorizedRedirect) {
-            this.oauthUnavailable = false
-            this.redirectUri = authorizedRedirect.proxy ? authorizedRedirect.proxy : authorizedRedirect.uri
-          } else {
-            this.oauthUnavailable = true
-            console.log(this.redirectUri + ' is not an authorized redirect URI for this application.')
-          }
+          this.oauthUnavailable = true
         }
       },
       error: (err) => { console.log(err) }
